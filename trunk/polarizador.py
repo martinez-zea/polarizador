3199bc148a7f37ec5925aeadb82e4b384fe1e367
cmart <devnull@localhost>
2008-06-24 04:15:00 -0700

    [svn r38]

--- trunk/polarizador.py
+++ trunk/polarizador.py
@@ -38,6 +38,7 @@ import time
 
 import imprimeTicket
 import habla
+import peticion
 
 h = habla.habla()
 m = imprimeTicket.imprimeTicket()
@@ -46,103 +47,49 @@ quienId = 0
 
 
 
-## Guarda el dato de quien llega y toma un id
-def guardaLlegada():
-	""" Guarda la hora de llegada de un visitante al tomar la tarjeta con el 
-	codigo de barras """
-	global quienId
-	quienId = quienId + 1	
-	
-	tiempo = datetime.datetime.now()
-	tiempoStr = tiempo.strftime("%Y-%m-%d-%H-%M-%S")
-	
-	db = MySQLdb.connect(host="localhost", user="root", passwd="", db="panoptico")
-	cursor = db.cursor()	
-	cursor.execute("INSERT INTO llega (quien,hora) VALUES (%s,%s)", (quienId,tiempoStr))
-	cursor.close()
-
-## Guarda el dato de la respuesta en la bd
-def guardaRespuesta(quien, respuesta):
-
-		db = MySQLdb.connect(host="localhost", user="root", passwd="", db="panoptico")
-		cursor = db.cursor()
-		cursor.execute("INSERT INTO responde (quien,respuesta) VALUES (%s,%s)", (quien,respuesta))
-		cursor.close
-
-################################################################################
-##############     PREGUNTAS A LA BD     #######################################
-################################################################################
-
-def cantaRespuesta1():
-	db2 = MySQLdb.connect(host="localhost", user="root", passwd="", db="panoptico") ##conexion a la bd
-	cursor2 = db2.cursor() ##crea cursor
-	cursor2.execute("SELECT LAST_INSERT_ID()  FROM responde") ##busca el ultimo registro
-	columnas2 = int(cursor2.rowcount) ##mira cuantos rows hay
-	cursor2.execute("SELECT *  FROM responde WHERE ID = %s" ,(columnas2))## hace el query preguntando por el ultimo registro del Id
-	row2 = cursor2.fetchone() ##mete el resultado en fetch one
-	print "ID: ", row2[0], "quien: ", row2[1], "resp: ", row2[2]
-	
-	h.que("El visistante, numero.")
-	h.que(str(row2[1]))
-	h.que("respondio que .")			
-	h.que(row2[2])	
-	
-def buscaRespuesta(que):
-	db1 = MySQLdb.connect(host="localhost", user="root", passwd="", db="panoptico") ##conexion a la bd
-	cursor1 = db1.cursor() ##crea cursor
-	cursor1.execute("SELECT quien, respuesta  FROM responde WHERE respuesta = %s ORDER BY rand()" ,(que)) ##busca el ultimo registro	
-	row1 = cursor1.fetchone() ##mete el resultado en fetch one
-	print row1[0], row1[1]
-	h.que("Uste respondio lo contrario al visitante numero")
-	h.que(str(row1[0]))
-	#h=habla.habla("espeak", "-ves",  "-s 135", row1[1])
-
-def buscaPares(que):
-	db1 = MySQLdb.connect(host="localhost", user="root", passwd="", db="panoptico") ##conexion a la bd
-	cursor1 = db1.cursor() ##crea cursor
-	cursor1.execute("SELECT respuesta  FROM responde WHERE respuesta = %s" ,(que)) ##busca el ultimo registro	
-	columnas = int(cursor1.rowcount)
-	return columnas
-	
 
 ################################################################################ Funcion Principal
 def polariza():
 	while 1:
 		print 'Este es el polarizador'
 
+
 		lector = serial.Serial('/dev/ttyUSB1', 9600, timeout=None)
 
 		codigo = str(lector.readline())
 		#print codigo
 		lector.close()
-
-		print codigo
+		print 'Usted es el visitante numero: ', codigo
+		print 'Presione un boton para responder la pregunta'
+	
+		
 		while len(codigo) > 0:
-			time.sleep(0.5)
+			#time.sleep(0.5)
 			h.que("Presione un boton, para contestar la pregunta")
 			#h=habla.habla("respondio que .")	
 			botones = serial.Serial('/dev/ttyUSB0', 9600, timeout=None)
 			bots = int(botones.readline())
 			botones.close() 
-		
+			p = peticion.peticion()
 
 			if bots == 2:
 				print "boton 2 presionado por ", codigo
 				#cantaRespuesta1()
-				guardaRespuesta(codigo, "si")
-				buscaRespuesta("no")
-				print str(buscaPares("si"))
+				
+				p.guardaRespuesta(codigo, "si")
+				p.buscaRespuesta("no")
+				print str(p.buscaPares("si"))
 				h.que("Imprimiendo")
-				m.imp(codigo,"SI",str(buscaPares("si")),"de acuerdo")
+				m.imp(codigo,"SI",str(p.buscaPares("si")),"de acuerdo")
 				break
 
 			if bots == 3:
 				print "boton 3 presionado por ", codigo
-				guardaRespuesta(codigo, "no")
-				buscaRespuesta("si")
-				print str(buscaPares("no"))
+				p.guardaRespuesta(codigo, "no")
+				p.buscaRespuesta("si")
+				print str(p.buscaPares("no"))
 				h.que("Imprimiendo")
-				m.imp(codigo,"NO",str(buscaPares("no")),"en desacuerdo")
+				m.imp(codigo,"NO",str(p.buscaPares("no")),"en desacuerdo")
 				break
 		#	print bots
 if __name__ == '__main__': polariza()
